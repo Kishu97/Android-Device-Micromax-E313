5d195ba1c2f0f977fe0ebda1be61052aacd820c6
Yashpal Joshi <shivomtechdroid@gmail.com>
2017-12-24 11:46:05 +0530

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,42 +2,45 @@
 
  cd ../../../..
  cd system/core
- patch -p1 < ../../device/Panasonic/P55Novo/patches/system_core.patch
+ patch -p1 < ../../device/Micromax/E313/patches/system_core.patch
  cd ..
  cd bt
- patch -p1 < ../../device/Panasonic/P55Novo/patches/system_bt.patch
+ patch -p1 < ../../device/Micromax/E313/patches/system_bt.patch
  cd ..
  cd netd
- patch -p1 < ../../device/Panasonic/P55Novo/patches/system_netd.patch
+ patch -p1 < ../../device/Micromax/E313/patches/system_netd.patch
  cd ..
  cd sepolicy
- patch -p1 < ../../device/Panasonic/P55Novo/patches/system_sepolicy.patch
+ patch -p1 < ../../device/Micromax/E313/patches/system_sepolicy.patch
  cd ..
  cd vold
- patch -p1 < ../../device/Panasonic/P55Novo/patches/system_vold.patch
+ patch -p1 < ../../device/Micromax/E313/patches/system_vold.patch
  cd ../..
  cd frameworks/av
- patch -p1 < ../../device/Panasonic/P55Novo/patches/frameworks_av.patch
+ patch -p1 < ../../device/Micromax/E313/patches/frameworks_av.patch
  cd ..
  cd base
- patch -p1 < ../../device/Panasonic/P55Novo/patches/frameworks_base.patch
+ patch -p1 < ../../device/Micromax/E313/patches/frameworks_base.patch
  cd ..
  cd native
- patch -p1 < ../../device/Panasonic/P55Novo/patches/frameworks_native.patch
+ patch -p1 < ../../device/Micromax/E313/patches/frameworks_native.patch
  cd ..
  cd opt/telephony
- patch -p1 < ../../../device/Panasonic/P55Novo/patches/frameworks_opt_telephony.patch
+ patch -p1 < ../../../device/Micromax/E313/patches/frameworks_opt_telephony.patch
  cd ../../..
  cd external/icu
- patch -p1 < ../../device/Panasonic/P55Novo/patches/external_icu.patch
+ patch -p1 < ../../device/Micromax/E313/patches/external_icu.patch
  cd ..
  cd wpa_supplicant_8
- patch -p1 < ../../device/Panasonic/P55Novo/patches/external_wpa_supplicant_8.patch
+ patch -p1 < ../../device/Micromax/E313/patches/external_wpa_supplicant_8.patch
  cd ../..
  cd vendor/codeaurora/telephony
- patch -p1 -p1 < ../../../device/Panasonic/P55Novo/patches/vendor_codeaurora_telephony.patch
+ patch -p1 -p1 < ../../../device/Micromax/E313/patches/vendor_codeaurora_telephony.patch
  cd ../../..
  cd packages/services/Telephony
- patch -p1 -p1 < ../../../device/Panasonic/P55Novo/patches/packages_services_Telephony.patch
+ patch -p1 -p1 < ../../../device/Micromax/E313/patches/packages_services_Telephony.patch
+ cd ../..
+ cd apps/Settings
+ patch -p1 -b < ../../../device/Micromax/E313/patches/author_info.patch
  cd ../../../
- echo Successfuly patched! Go ahead and start building!
+echo Successfuly patched! Go ahead and start building! Thanks Yashpal Joshi 
